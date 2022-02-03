e7fa1156eaa4c7f2f92c40c61322fe23d00dc178
Anton Kolesnyk <antkmsft@users.noreply.github.com>
2022-02-02 17:35:02 -0800

    Nov 2021 + Jan 2022: core 1.3.1, storage-blobs 12.2.1, storage-queues beta2, identity 1.1.1, keyvault-certificates beta1

--- ports/azure-security-keyvault-certificates-cpp/portfile.cmake
+++ ports/azure-security-keyvault-certificates-cpp/portfile.cmake
@@ -1,12 +1,12 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Azure/azure-sdk-for-cpp
-    REF azure-storage-queues_12.0.0-beta.2
-    SHA512 43815483aa8def6eb475683d765a5315a65218bceb9dbf95b9f4e154c23ed8af1af4a52943b4b89fed35b50318407c98674e8af4c4076e42be60f460a610a118
+    REF azure-security-keyvault-certificates_4.0.0-beta.1
+    SHA512 8cb7fccade755cb984a311430a808e777a041e663b607a40308a79b353431abea2eaef1c49c48c783ec9020b5d0e6e53fa101f2e2432bd71bba3b578bfe810ca
 )
 
 vcpkg_cmake_configure(
-    SOURCE_PATH ${SOURCE_PATH}/sdk/storage/azure-storage-queues/
+    SOURCE_PATH ${SOURCE_PATH}/sdk/keyvault/azure-security-keyvault-certificates/
     OPTIONS
         -DWARNINGS_AS_ERRORS=OFF
 )
