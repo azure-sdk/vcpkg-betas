674ef0a8554485c0caf21a0d7c95e7f6ca1cddc4
Anton Kolesnyk <antkmsft@users.noreply.github.com>
2022-02-05 01:28:03 -0800

    Feb 4 2022: core 1.4.0-beta.1

--- ports/azure-core-cpp/portfile.cmake
+++ ports/azure-core-cpp/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Azure/azure-sdk-for-cpp
-    REF azure-core_1.3.1
-    SHA512 affdd6387b5b40f0362d4151f91b1a0f3d0269189240baf981e31a9a1860b77d7fcfeb7819f93f9b1e5764ab1ba2190cf1c6261f23345e0928453550f8537de1
+    REF azure-core_1.4.0-beta.1
+    SHA512 27c028d1f9f4b0d36a09773b0412153126539f72976f38dc5869bcc38feae7083d62951e8b76b17a952ab7b00e4d9ad21b5fceb67e39b2ffb8bcf74d486962e1
 )
 
 vcpkg_check_features(
